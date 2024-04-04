a5865d066da4942928e25326575a6fb3a8890fc3
Anurag <hamsen3880@gmail.com>
2024-04-04 22:12:16 +0530

    242 Java

---
+++ Algorithms/Java/Easy/_0242_Valid_Anagram.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class _0242_Valid_Anagram {
+    static boolean isAnagram(String s, String t) {
+        char arrayS[] = s.toCharArray();
+        char arrayT[] = t.toCharArray();
+        Arrays.sort(arrayS);
+        Arrays.sort(arrayT);
+        s = new String(arrayS);
+        t = new String(arrayT);
+        if(s.equals(t)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        sc.close();
+        if(isAnagram(str1, str2)) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
+
+// HASH TABLE | STRING | SORTING
+
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
