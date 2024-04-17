51a4fe1bb2269afe96c221256a63122bdcab53b9
Anurag <hamsen3880@gmail.com>
2024-04-17 11:21:35 +0530

    1 C++

---
+++ Algorithms/C++/Easy/_0001_Two_Sum.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> answer(2);
+    for(int i = 0; i < nums.size() - 1; i++) {
+        for(int j = i + 1; j < nums.size(); j++) {
+            if(nums[i] + nums[j] == target) {
+                answer[0] = i;
+                answer[1] = j;
+                break;
+            }
+        }
+    }
+    return answer;
+}
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    int target;
+    cin >> target;
+    vector<int> answer = twoSum(nums, target);
+    for(int i = 0; i < answer.size(); i++) {
+        cout << answer[i] << " ";
+    }
+    return 0;
+}
+
+// ARRAY | HASH TABLE
+
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
