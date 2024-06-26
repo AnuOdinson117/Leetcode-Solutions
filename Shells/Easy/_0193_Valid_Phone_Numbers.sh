aca6209e88eb0763b157daec913067c378ee7b61
Anurag <hamsen3880@gmail.com>
2024-06-27 01:50:15 +0530

    193 Bash

--- Shells/Easy/_0193_Valid_Phone_Numbers.sh
+++ Shells/Easy/_0193_Valid_Phone_Numbers.sh
@@ -1,1 +1,12 @@
 grep -E '^(\([0-9]{3}\) [0-9]{3}-[0-9]{4}|[0-9]{3}-[0-9]{3}-[0-9]{4})$' number.txt
+
+# SHELL
+
+# Example:
+# Assume that file.txt has the following content:
+# 987-123-4567
+# 123 456 7890
+# (123) 456-7890
+# Your script should output the following valid phone numbers:
+# 987-123-4567
+# (123) 456-7890
