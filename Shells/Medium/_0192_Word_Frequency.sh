d5831c8764b15b19cbc1df5fff7e0273ff3488ed
Anurag <hamsen3880@gmail.com>
2024-06-27 01:50:25 +0530

    192 Bash

--- Shells/Medium/_0192_Word_Frequency.sh
+++ Shells/Medium/_0192_Word_Frequency.sh
@@ -1,1 +1,13 @@
 cat words.txt | tr -s " " "\n" | sort | uniq -c | sort -r | awk '{print $2, $1}'
+
+# SHELL
+
+# Example:
+# Assume that words.txt has the following content:
+# the day is sunny the the
+# the sunny is is
+# Your script should output the following, sorted by descending frequency:
+# the 4
+# is 3
+# sunny 2
+# day 1
