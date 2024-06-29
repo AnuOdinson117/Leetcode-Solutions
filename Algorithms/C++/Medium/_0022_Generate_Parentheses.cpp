9561fc697ffd3bc419ab62e3e6c71017569663e8
Anurag <hamsen3880@gmail.com>
2024-06-29 06:37:46 +0530

    22 C++

---
+++ Algorithms/C++/Medium/_0022_Generate_Parentheses.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void generate(vector<string>& ans, string s, int open, int close, int n) {
+    if(close == n) {
+        ans.push_back(s);
+        return;
+    }
+    if(open < n) {
+        generate(ans, s + '(', open + 1, close, n);
+    }
+    if(close < open) {
+        generate(ans, s + ')', open, close + 1, n);
+    }
+}
+
+vector<string> generateParenthesis(int n) {
+    vector<string> ans;
+    generate(ans, "", 0, 0, n);
+    return ans;
+}
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<string> ans = generateParenthesis(n);
+    for(int i = 0; i < ans.size(); i++) {
+        cout << ans[i];
+    }
+    return 0;
+}
